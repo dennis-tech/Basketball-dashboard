fcb8e035216fbdcf12341aec9fd544a19dbe4f51
dennis-tech <hedenniskibe@gmail.com>
2023-06-09 18:33:36 +0300

    refactored js, html and css

--- index.js
+++ index.js
@@ -1,58 +1,67 @@
 // getting values from html
+
 let gameScoreHome = document.getElementById("home-p");
 let gameScoreAway = document.getElementById("guest-p");
 
-// HOMESCORES
+gameScoreHome.innerHTML = 0;
+gameScoreAway.innerHTML = 0;
+
+document.getElementById('values-1').addEventListener('click',addOne);
+document.getElementById('values-2').addEventListener('click',addTwo);
+document.getElementById('values-3').addEventListener('click',addThree);
+document.getElementById('values-4').addEventListener('click',addOne4);
+document.getElementById('values-5').addEventListener('click',addTwo5);
+document.getElementById('values-6').addEventListener('click',addThree6);
+document.getElementById("game-btn").addEventListener("click", reset);
+
+// homeScores
+
 function addOne() {
     gameScoreHome.innerHTML = parseInt(gameScoreHome.innerHTML) + 1;
     return gameScoreHome.innerHTML;
+    
 }
+
 function addTwo() {
     gameScoreHome.innerHTML = parseInt(gameScoreHome.innerHTML) + 2;
     return gameScoreHome.innerHTML;
 }
+
 function addThree() {
     gameScoreHome.innerHTML = parseInt(gameScoreHome.innerHTML) + 3;
     return gameScoreHome.innerHTML;
+    
 }
 
-// AWAYSCORES
+// awayScores
 
 function addOne4() {
     gameScoreAway.innerHTML = parseInt(gameScoreAway.innerHTML) + 1;
     return gameScoreAway.innerHTML;
     
 }
+
 function addTwo5() {
     gameScoreAway.innerHTML = parseInt(gameScoreAway.innerHTML) + 2;
     return gameScoreAway.innerHTML;
     
 }
+
 function addThree6() {
     gameScoreAway.innerHTML = parseInt(gameScoreAway.innerHTML) + 3;
     return gameScoreAway.innerHTML;
     
 }
 
-// RESET
-let newGame = document.getElementById("game-btn");
-newGame.addEventListener("click", function(){
-    scoreHome = 0;
-    scoreAway = 0;
-    gameScoreHome.innerHTML = scoreHome;
-    gameScoreAway.innerHTML = scoreAway;
-});
+// reset
 
-// // HIGHLIGHTING LEADERBOARD
+function reset(){
+    gameScoreHome.innerHTML = 0;
+    gameScoreAway.innerHTML = 0;
+}
+console.log(gameScoreHome.innerHTML, gameScoreAway.innerHTML);
 
-// if (gameScoreHome.innerHTML > gameScoreAway.innerHTML) {
-//     gameScoreHome.style.backgroundColor = "green";
-//     gameScoreAway.style.backgroundColor = "red";
-// } else if(scoreHome < gameScoreAway.innerHTML) {
-//     gameScoreHome.style.backgroundColor = "red";
-//     gameScoreAway.style.backgroundColor = "green";
-    
-// } else {
-//     gameScoreHome.style.backgroundColor = "white";
-//     gameScoreAway.style.backgroundColor = "white";
-// }
+
+
+
+
